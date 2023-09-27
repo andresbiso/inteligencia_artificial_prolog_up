aeca34f33988d03a580436615c7340e8e03a1497
Andrés Biso <biso.andres@gmail.com>
2023-09-26 22:19:56 -0300

    Update unidad_4_actividad_ejercicio_2.pl

--- unidad_4_actividad_ejercicio_2.pl
+++ unidad_4_actividad_ejercicio_2.pl
@@ -1,51 +1,15 @@
-%# hechos
-%## predicados simples
-man(peter).
-man(juan).
-man(horacio).
-woman(eve).
-woman(juana).
-%## predicados compuestos
-father(peter, juan).
-mother(eve, juan).
+% Ejercicio 2
+/*
+ * Dado los siguientes hechos, crear una regla que defina depredador y otra presa.
+ */
+% hechos
+is_animal(perro).
+is_animal(canario).
+is_animal(gato).
+is_animal(delfin).
+is_animal(mojarrita).
+come(gato, canario).
+come(perro, gato).
+come(delfin, mojarrita).
 
-%# reglas
-is_man(X) :- man(X).
-has_child(P) :- father(P, _).
-is_father(P) :- man(P) , has_child(P).
-is_child(C) :- father(_, C) ; mother(_, C).
-
-%# consultas al motor de inferencias
-% man(juan). -> true
-% man(jonatan). -> false
-% father(peter, juan). -> true
-
-%# consulta con variable
-% man(X).
-% X = peter
-% X = juan
-% X = horacio
-%! valores de X para que el predicado sea verdadero
-% father(peter,X).
-% X = juan
-
-%# consulta con reglas
-% is_man(juan). -> true
-% has_child(peter). -> true
-% has_child(X).
-% X = peter
-% is_father(peter). -> true
-% is_child(C).
-% C = juan
-% C = juan
-%! aparece dos veces porque es un OR y se cumple para dos hechos
-
-%# comentarios
-%- con ctrl + enter corremos las consultas
-%- con "." indicamos fin de línea
-%- las variables van en mayúscula
-%- los hechos forman la base de conocimientos del programa
-%- podemos pensar las reglas como si fuesen funciones de proposiciones
-%- "_": cualquier valor
-%- ",": AND
-%- ";": OR
+% reglas
