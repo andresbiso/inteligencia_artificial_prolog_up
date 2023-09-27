efc1fdc57081648899b9e11d275a7dc9762e0ab2
Andrés Biso <biso.andres@gmail.com>
2023-09-26 23:43:55 -0300

    Update unidad_4_actividad_ejercicio_4.pl

--- unidad_4_actividad_ejercicio_4.pl
+++ unidad_4_actividad_ejercicio_4.pl
@@ -1,51 +1,46 @@
-%# hechos
-%## predicados simples
-man(peter).
-man(juan).
-man(horacio).
-woman(eve).
-woman(juana).
-%## predicados compuestos
-father(peter, juan).
-mother(eve, juan).
+% Ejercicio 3
+/*
+ * Crear un programa en Prolog que represente la base de conocimiento
+ * de un árbol genealógico de tu preferencia.
+ * Deben contener al menos 20 hechos y 5 reglas. 
+ */
 
-%# reglas
-is_man(X) :- man(X).
-has_child(P) :- father(P, _).
-is_father(P) :- man(P) , has_child(P).
-is_child(C) :- father(_, C) ; mother(_, C).
+% hechos
+hombre(luis).
+hombre(jorge).
+hombre(esteban).
+hombre(matias).
+hombre(tomas).
+hombre(sebastian).
 
-%# consultas al motor de inferencias
-% man(juan). -> true
-% man(jonatan). -> false
-% father(peter, juan). -> true
+mujer(florencia).
+mujer(julieta).
+mujer(viviana).
+mujer(norma).
 
-%# consulta con variable
-% man(X).
-% X = peter
-% X = juan
-% X = horacio
-%! valores de X para que el predicado sea verdadero
-% father(peter,X).
-% X = juan
+pareja(luis, florencia).
+pareja(esteban, julieta).
+pareja(matias, viviana).
 
-%# consulta con reglas
-% is_man(juan). -> true
-% has_child(peter). -> true
-% has_child(X).
-% X = peter
-% is_father(peter). -> true
-% is_child(C).
-% C = juan
-% C = juan
-%! aparece dos veces porque es un OR y se cumple para dos hechos
+progenitor(luis, jorge).
+progenitor(luis, esteban).
+progenitor(esteban, matias).
+progenitor(matias, tomas).
+progenitor(matias, sebastian).
+progenitor(matias, norma).
 
-%# comentarios
-%- con ctrl + enter corremos las consultas
-%- con "." indicamos fin de línea
-%- las variables van en mayúscula
-%- los hechos forman la base de conocimientos del programa
-%- podemos pensar las reglas como si fuesen funciones de proposiciones
-%- "_": cualquier valor
-%- ",": AND
-%- ";": OR
+progenitor(florencia, jorge).
+progenitor(florencia, esteban).
+progenitor(julieta, matias).
+progenitor(viviana, tomas).
+progenitor(viviana, sebastian).
+progenitor(viviana, norma).
+
+% reglas
+es_padre(X,Y) :- hombre(X) , progenitor(X, Y).
+es_madre(X,Y) :- mujer(X) , progenitor(X,Y).
+son_hermanos(X,Y) :- progenitor(Z,X) , progenitor(Z,Y) , X\=Y.
+es_esposo(X,Y) :- pareja(X,Y) , hombre(X).
+es_esposa(X,Y) :- pareja(Y,X) , mujer(X).
+es_abuelo(X,Y) :- progenitor(Z,Y) , es_padre(X,Z).
+es_abuela(X,Y) :- progenitor(Z,Y) , es_madre(X,Z).
